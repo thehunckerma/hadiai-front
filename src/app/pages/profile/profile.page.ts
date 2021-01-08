0b6d3ca1125a8edfe3d411b9869b746d7239df98
Hyungk <moulahidkawtar@email.com>
2021-01-09 00:11:36 +0100

    updated version of profile /static

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+
+import { Plugins } from '@capacitor/core';
+const { Storage } = Plugins;
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  credentials: FormGroup;
+constructor(){}
+  ngOnInit(){
+    
   }
 
+ 
+  
 }
+
+
+
