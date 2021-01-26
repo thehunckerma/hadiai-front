55c9e2910570d5c1902f84b04f870b31ca3addeb
kawtar <kawtar.moulahid@uit.ac.ma>
2021-01-26 14:29:05 +0100

    add some icons , fix profile page

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,17 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../../services/authentication.service';
+import { Router } from '@angular/router';
 
+import { Plugins } from '@capacitor/core';
+const { Storage } = Plugins;
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
-  user = {
-    id: 0,
-    username: 'test',
-    email: 'test',
+  shouldLoad = false;
+  hiddenPassword: string;
+  credentials: {
+    fullname: string;
+    email: string;
+    username: string;
+    password: string;
+    image?: string;
   };
-  constructor() {}
-
+  constructor(    
+    private authService: AuthenticationService,
+    private router: Router,
+    ) {
+    Storage.get({ key: 'stringified-creds' })
+      .then((storage: { value: string }) => {
+        const stringifiedCreds = storage.value;
+        this.credentials = JSON.parse(stringifiedCreds);
+        if (!this.credentials) {
+          this.router.navigateByUrl('/signup', { replaceUrl: true });
+        } else {
+          this.hiddenPassword = '*'.repeat(this.credentials.password.length);
+          this.shouldLoad = true;
+        }
+      })
+      .catch((err: any) =>
+        this.router.navigateByUrl('/signup', { replaceUrl: true })
+      );
+    }
   ngOnInit() {}
 }
