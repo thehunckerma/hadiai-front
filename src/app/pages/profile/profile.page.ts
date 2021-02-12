ed5d4e012947947339b322c6bc9b86f24c353bce
kawtar <kawtar.moulahid@uit.ac.ma>
2021-02-12 17:00:09 +0100

    Add profile page

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -10,33 +10,8 @@ const { Storage } = Plugins;
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
-  shouldLoad = false;
-  hiddenPassword: string;
-  credentials: {
-    fullname: string;
-    email: string;
-    username: string;
-    password: string;
-    image?: string;
-  };
-  constructor(    
-    private authService: AuthenticationService,
-    private router: Router,
-    ) {
-    Storage.get({ key: 'stringified-creds' })
-      .then((storage: { value: string }) => {
-        const stringifiedCreds = storage.value;
-        this.credentials = JSON.parse(stringifiedCreds);
-        if (!this.credentials) {
-          this.router.navigateByUrl('/signup', { replaceUrl: true });
-        } else {
-          this.hiddenPassword = '*'.repeat(this.credentials.password.length);
-          this.shouldLoad = true;
-        }
-      })
-      .catch((err: any) =>
-        this.router.navigateByUrl('/signup', { replaceUrl: true })
-      );
-    }
+ 
+  constructor() {}
+   
   ngOnInit() {}
 }
